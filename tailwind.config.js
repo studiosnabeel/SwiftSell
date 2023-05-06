ecaf0251ce4eeb0f67e4b9cbb193ca01766a238c
Nabeel Akbar <studiosnabeel@gmail.com>
2023-05-06 16:30:14 +0500

    color templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bodyFont: 'poppins',
+        titleFont: 'Source Code Pro',
+      },
+      colors: {
+        primary: '#8c52ff',
+        secondary: 'RGB(118 119 122)',
+      },
+    },
   },
   plugins: [],
 };
