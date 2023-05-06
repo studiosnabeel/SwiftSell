dc776822fd5d925443b53d682c8ff150f84985cf
Nabeel Akbar <studiosnabeel@gmail.com>
2023-05-06 16:28:15 +0500

    Add images to index.js

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import logo from './logo1.png';
+import github from './github.png';
+import payment from './payment.png';
+import cart from './cart.png';
+import google from './google.png';
+import cartlogo from './cartlogo.png';
+
+export { logo, github, payment, cart, cartlogo, google };
