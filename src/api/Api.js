f6eda38e3ddc789100cdd94ee9727e76bf61a8a0
Nabeel Akbar <studiosnabeel@gmail.com>
2023-05-09 20:17:58 +0500

    persist store

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function productsData() {
   const products = await axios.get(
-    'https://fakestoreapiserver.reactbd.com/amazonproducts'
+    'https://fakestoreapiserver.reactbd.com/products'
   );
   return products;
 }
---
+++ src/redux/swiftSlice.js
@@ -0,0 +1,65 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  productData: [],
+  userInfo: null,
+};
+
+export const swiftSlice = createSlice({
+  name: 'swift',
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = state.productData.find(
+        (item) => item._id === action.payload._id
+      );
+      if (item) {
+        item.quantity += action.payload.quantity;
+      } else {
+        state.productData.push(action.payload);
+      }
+    },
+    deleteItem: (state, action) => {
+      state.productData = state.productData.filter(
+        (item) => item._id !== action.payload
+      );
+    },
+    resetCart: (state) => {
+      state.productData = [];
+    },
+    incrementQuantity: (state, action) => {
+      const item = state.productData.find(
+        (item) => item._id === action.payload._id
+      );
+      if (item) {
+        item.quantity += 1;
+      }
+    },
+    decrementQuantity: (state, action) => {
+      const item = state.productData.find(
+        (item) => item._id === action.payload._id
+      );
+      if (item) {
+        item.quantity -= 1;
+      }
+    },
+    addUser: (state, action) => {
+      state.userInfo = action.payload;
+    },
+    removeUser: (state) => {
+      state.userInfo = null;
+    },
+  },
+});
+
+export const {
+  addToCart,
+  deleteItem,
+  resetCart,
+  incrementQuantity,
+  decrementQuantity,
+  addUser,
+  removeUser,
+} = swiftSlice.actions;
+
+export default swiftSlice.reducer;
