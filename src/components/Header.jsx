e6959c60e4ce98e539cea872c1ebe40d37b207aa
Nabeel Akbar <studiosnabeel@gmail.com>
2023-05-09 20:14:25 +0500

    Modify Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { logo, cartlogo } from '../assets/index';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const productData = useSelector((state) => state.swift.productData);
+  const userInfo = useSelector((state) => state.swift.userInfo);
   return (
     <div
       className="w-full h-20 bg-white border-b-[1px] border-b-primary flex justify-between items-center px-14 font-titleFont sticky top-0 z-50
     "
     >
       <div>
-        <div
-          className="max-w-screen-xl h-full mx-auto flex items-center justify-between
+        <Link to="/">
+          <div
+            className="max-w-screen-xl h-full mx-auto flex items-center justify-between
         "
-        >
-          <img className="w-16" src={logo} alt="Logo SwiftSell" />
-        </div>
+          >
+            <img className="w-16" src={logo} alt="Logo SwiftSell" />
+          </div>
+        </Link>
       </div>
       <div className="flex gap-8">
         <ul className="flex justify-center items-center gap-8">
@@ -48,13 +54,22 @@ const Header = () => {
             Blog
           </li>
         </ul>
-        <div className="relative flex justify-center align-middle w-8 cursor-pointer duration-300">
-          <img className="w-6" src={cartlogo} alt="cart" />
-          <span className="absolute w-6 top-1 left-1 text-sm flex item-center justify-center font-semibold font-titleFont text-white">
-            0
-          </span>
-        </div>
-        <img className="w-6 h-6 rounded-full" src={logo} alt="user-logo" />
+        <Link to="/cart">
+          <div className="relative flex justify-center align-middle w-8 cursor-pointer duration-300">
+            <img className="w-6" src={cartlogo} alt="cart" />
+            <span className="absolute w-6 top-1 left-1 text-sm flex item-center justify-center font-semibold font-titleFont text-white">
+              {productData.length}
+            </span>
+          </div>
+        </Link>
+        <Link to="/login">
+          <img
+            className="w-6 h-6 rounded-full"
+            src={userInfo ? userInfo.image : logo}
+            alt="Sign-in"
+          />
+        </Link>
+        {userInfo && <p className="text-primary ">{userInfo.name}</p>}
       </div>
     </div>
   );
